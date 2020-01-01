fda10b853f0bf46b5c5eec90350eb2fb94e4710a
onlyreg43 <59367473+onlyreg43@users.noreply.github.com>
2020-01-01 16:16:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,22 +25,4 @@ resource "aws_instance" "default" {
   }
 }
 
-#Create Security Group for EC2
-resource "aws_security_group" "default" {
-  name = "terraform-default-sg1"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-}
