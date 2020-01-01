fb0f222466d5e350c6413750a66abbe9ac8cfbfc
onlyreg43 <59367473+onlyreg43@users.noreply.github.com>
2020-01-01 21:57:20 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,49 +1,49 @@
 output "this_db_instance_address" {
   description = "The address of the RDS instance"
-  value       = module.db.this_db_instance_address
+  value       = aws_db_instance.this_db_instance_address
 }
 
 output "this_db_instance_arn" {
   description = "The ARN of the RDS instance"
-  value       = module.db.this_db_instance_arn
+  value       = aws_db_instance.this_db_instance_arn
 }
 
 output "this_db_instance_endpoint" {
   description = "The connection endpoint"
-  value       = module.db.this_db_instance_endpoint
+  value       = aws_db_instance.this_db_instance_endpoint
 }
 
 output "this_db_instance_id" {
   description = "The RDS instance ID"
-  value       = module.db.this_db_instance_id
+  value       = aws_db_instance.this_db_instance_id
 }
 
 output "this_db_instance_resource_id" {
   description = "The RDS Resource ID of this instance"
-  value       = module.db.this_db_instance_resource_id
+  value       = aws_db_instance.this_db_instance_resource_id
 }
 
 output "this_db_instance_status" {
   description = "The RDS instance status"
-  value       = module.db.this_db_instance_status
+  value       = aws_db_instance.this_db_instance_status
 }
 
 output "this_db_instance_name" {
   description = "The database name"
-  value       = module.db.this_db_instance_name
+  value       = aws_db_instance.this_db_instance_name
 }
 
 output "this_db_instance_username" {
   description = "The master username for the database"
-  value       = module.db.this_db_instance_username
+  value       = aws_db_instance.this_db_instance_username
 }
 
 output "this_db_instance_password" {
   description = "The database password (this password may be old, because Terraform doesn't track it after initial creation)"
-  value       = module.db.this_db_instance_password
+  value       = aws_db_instance.this_db_instance_password
 }
 
 output "this_db_instance_port" {
   description = "The database port"
-  value       = module.db.this_db_instance_port
+  value       = aws_db_instance.this_db_instance_port
 }
